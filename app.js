695160631bb15091bc085e9d10b43b7abf9d49ae
Thomas Syczyk <tomsycz@gmail.com>
2021-02-11 21:57:28 +0000

    Improved stripe implementation 5

--- app.js
+++ app.js
@@ -62,11 +62,7 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.post(
-  '/webhook-checkout',
-  express.raw({ type: 'application/json' }),
-  bookingController.webhookCheckout
-);
+app.post('/webhook-checkout', express.raw(), bookingController.webhookCheckout);
 
 // Body parser, reading data form body into req.body
 app.use(express.json({ limit: '10kb' }));
